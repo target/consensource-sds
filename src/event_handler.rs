0a39426278d220982580ddaf661e2744be13767c
Trevor McDonald <trevor.mcdonald@target.com>
2020-11-02 15:47:56 -0600

    Use new assertion delete operation with address and block num
    Signed-off-by: Trevor McDonald <trevor.mcdonald@target.com>

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -183,10 +183,9 @@ impl EventHandler {
                     Ok(transaction)
                 }
                 StateChange_Type::DELETE => {
-                    let assertion_container: assertion::AssertionContainer =
-                        Self::unpack_data(state.get_value());
                     let transaction = OperationType::DeleteAssertion(
-                        assertion_container.to_models(block.block_num),
+                        state.get_address().to_string(),
+                        block.block_num,
                     );
                     Ok(transaction)
                 }
@@ -417,6 +416,7 @@ impl FromStateAtBlock<assertion::Assertion> for NewAssertion {
     fn at_block(block_num: i64, assertion: &assertion::Assertion) -> Self {
         NewAssertion {
             assertion_id: assertion.get_id().to_string(),
+            address: assertion.get_address().to_string(),
             assertor_pub_key: assertion.get_assertor_pub_key().to_string(),
             assertion_type: match assertion.get_assertion_type() {
                 assertion::Assertion_Type::STANDARD => AssertionTypeEnum::Standard,
@@ -448,6 +448,7 @@ mod tests {
     const REQUEST_ID: &str = "test_request";
     const STANDARD_ID: &str = "test_standard";
     const ASSERTION_ID: &str = "test_assertion";
+    const ASSERTION_ADDRESS: &str = "some_state_address";
 
     #[test]
     /// Test that FromStateAtBlock::at_block returns a valid cert body, accreditation, auth, and contact
@@ -614,6 +615,7 @@ mod tests {
     fn test_assertion_at_block() {
         let new_assertion = NewAssertion {
             assertion_id: ASSERTION_ID.to_string(),
+            address: ASSERTION_ADDRESS.to_string(),
             assertor_pub_key: PUBLIC_KEY.to_string(),
             assertion_type: AssertionTypeEnum::Factory,
             object_id: FACTORY_ID.to_string(),
@@ -735,6 +737,7 @@ mod tests {
     fn make_assertion() -> assertion::Assertion {
         let mut assertion = assertion::Assertion::new();
         assertion.set_id(ASSERTION_ID.to_string());
+        assertion.set_address(ASSERTION_ADDRESS.to_string());
         assertion.set_assertor_pub_key(PUBLIC_KEY.to_string());
         assertion.set_assertion_type(assertion::Assertion_Type::FACTORY);
         assertion.set_object_id(FACTORY_ID.to_string());
